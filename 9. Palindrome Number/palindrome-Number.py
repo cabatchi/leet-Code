44cc190f31ec1a68d7b1a8342a32ac7731509cde
cabatchi <37081354+cabatchi@users.noreply.github.com>
2022-09-13 20:14:18 -0700

    yeah!!!!

---
+++ 9. Palindrome Number/palindrome-Number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        
+        x: int
+        rtype: bool
+
+        x = 121
+
+        str(x) = inP 
+
+        inPInv = inP[::-1]
+
+        print(inPInv)
+    
